f6001437eaeb973f55089df7d08a834335347a6e
Rose Blackman <blackman.rose@gmail.com>
2019-10-07 14:40:04 -0400

    second commit exercise 16 - post rachelle's help!

--- 16_FileIO_Reading_in/pair-exercise/file-io-part1-exercises-pair/Program.cs
+++ 16_FileIO_Reading_in/pair-exercise/file-io-part1-exercises-pair/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 
 
 namespace file_io_part1_exercises_pair
@@ -17,34 +18,45 @@ namespace file_io_part1_exercises_pair
             int sumOfWords = 0;
             int sumOfSentences = 0;
 
+            string fullPath = Path.Combine(userInputDirectory, userInputFileName);
             try
             {
-
-                string fullPath = Path.Combine(userInputDirectory, userInputFileName);
-                using (StreamReader sr = new StreamReader(fullPath)) 
+             
+                
+                using (StreamReader sr = new StreamReader(fullPath))
                 {
-                    string allWords = sr.ToString();
-                    string [] wordCountString = allWords.Split(" ");
-                    //while(!sr.EndOfStream)
-                    //{
-                    //    sumOfWords++;
-                    //}
-                    for(int i = 0; i < wordCountString.Length; i++)
+                    while (!sr.EndOfStream)
                     {
-                        sumOfWords++;
+                        string allWords = sr.ReadLine();
+                        //string allWords = sr.ToString();
+                        string[] wordCountString = allWords.Split(" ");
+                        //List<string> wordCountString = new List<string>();
+                        //wordCountString = allWords.Split(" ")
+
+                        //while(!sr.EndOfStream)
+
+                        //    sumOfWords++;
+                        //}
+                        for (int i = 0; i < wordCountString.Length; i++)
+                        {
+                            sumOfWords = sumOfWords + 1;
+                        }
+
+
+
+                        //char[] delimiterChars = { '.', '!', '?' };
+                        //string allSentences = sr.ToString();
+                        //string[] sentenceCountString = allSentences.Split(delimiterChars);
+                        ////while(!sr.EndOfStream)
+                        ////{
+                        ////    sumOfSentences++;
+                        ////}   
+                        //for (int i = 0; i < sentenceCountString.Length; i++)
+                        //{
+                        //    sumOfSentences = sumOfSentences + 1;
+                        //}
                     }
 
-                    char[] delimiterChars = { '.', '!', '?' };
-                    string allSentences = sr.ToString();
-                    string[] sentenceCountString = allSentences.Split(delimiterChars);
-                    //while(!sr.EndOfStream)
-                    //{
-                    //    sumOfSentences++;
-                    //}   
-                    for (int i = 0; i < sentenceCountString.Length; i++)
-                    {
-                        sumOfSentences++;
-                    }
                 }
 
             }
@@ -55,7 +67,7 @@ namespace file_io_part1_exercises_pair
             }
 
             Console.WriteLine("The total number of words used in the file is " + sumOfWords + ".");
-            Console.WriteLine("The total number of sentences used in the file is " + sumOfSentences + ".");
+           // Console.WriteLine("The total number of sentences used in the file is " + sumOfSentences + ".");
             Console.ReadLine();
 
             //C:\Users\roseb\team4-c-sharp-week4-pair-exercises\16_FileIO_Reading_in\pair-exercise
