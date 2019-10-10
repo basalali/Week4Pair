9688e665a13e6b4f61b6911028a20518cf7e12c9
John Fulton <john@techelevator.com>
2019-10-10 07:46:47 -0400

    19_Mini-Capstone/

---
+++ 19_Mini-Capstone/Capstone/Classes/FileAccess.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Classes
+{
+    class FileAccess
+    {
+        // This class should contain any and all details of access to files
+    }
+}
