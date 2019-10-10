861276ca6f058ccf44eea0af8130f5898ab1902a
Rose Blackman <blackman.rose@gmail.com>
2019-10-10 11:32:59 -0400

    mini capstone start - userinterface

--- 19_Mini-Capstone/Capstone/Classes/UserInterface.cs
+++ 19_Mini-Capstone/Capstone/Classes/UserInterface.cs
@@ -18,9 +18,79 @@ namespace Capstone.Classes
             while (!done)
             {
                 Console.WriteLine("This is the UserInterface");
+                Console.WriteLine("Press any key to continue.");
                 Console.ReadLine();
+                done = true;
             }
+            // StockInventory(); Within this method, read inventory txt and add to data (all items should be at 50)
 
+            PrintInitialMenu();
+
+            string initialSelection = Console.ReadLine();
+            string orderSelection = Console.ReadLine();
+            while (initialSelection != "3")
+                {
+                switch (initialSelection)
+                {
+                    case "1":
+                        DisplayCateringItems();
+                        break;
+                    case "2":
+                        Order();
+                        OrderSelection(orderSelection);
+                        break;
+                    default:
+                        Console.WriteLine();
+                        Console.WriteLine("Please make a valid selection.");
+                        Console.WriteLine();
+                        break;
+                }
+            }           
+        }
+        private void PrintInitialMenu()
+        {
+            Console.WriteLine("Please enter a choice:");
+            Console.WriteLine("1 - Display Catering Items");
+            Console.WriteLine("2 - Order");
+            Console.WriteLine("3 - Quit");
+            return;
+        }
+
+        private void DisplayCateringItems()
+        {
+            while()
+            {
+
+            }
+        }
+
+        private void Order()
+        {
+            Console.WriteLine("1 - Add money");
+            Console.WriteLine("2 - Select products");
+            Console.WriteLine("3 - Complete Transaction");
+            return;
+        }
+
+        private void OrderSelection(string orderSelection)
+        {
+            while (orderSelection != "3")
+            {
+                switch (orderSelection)
+                {
+                    case "1":
+                        //AddMoney();
+                        break;
+                    case "2":
+                        //SelectProducts();
+                        break;
+                    default:
+                        Console.WriteLine();
+                        Console.WriteLine("Please make a valid selection.");
+                        Console.WriteLine();
+                        break;
+                }
+            }
         }
     }
 }
